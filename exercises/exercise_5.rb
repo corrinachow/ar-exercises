46528f9d7a2277ba072a5e3c0961b81f1d696c52
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-22 17:47:15 +0000

    Add faker and employees

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -3,8 +3,16 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
+require 'faker'
 
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: 60)
+@store1.employees.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: 60)
+@store1.employees.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: 60)
+@store1.employees.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: 60)
+@store1.employees.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: 60)
