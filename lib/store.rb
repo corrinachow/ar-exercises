7d7765351b108a212eab477b0ca129e4a45ec640
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-22 18:18:25 +0000

    Add store validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
-  # validates :name, length: { minimum: 3 }
-  # validates :annual_revenue, numericality: { other_than: 0 }
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { other_than: 0 }
 
 end
