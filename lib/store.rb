cb0c0f4c9412a23fc4564733688ce5a7f8ba15dd
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-22 19:01:14 +0000

    Complete ex 7 and bonus

--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
 
-  validates :name, length: { minimum: 3 }
-  validates :annual_revenue, numericality: { other_than: 0 }
+  validates :name, length: { minimum: 3 }, presence: true
+  validates :annual_revenue, numericality: { other_than: 0 }, presence: true
+  validate :apparel_validator
+
+  def apparel_validator
+    if !mens_apparel && !womens_apparel
+      errors.add(:womens_apparel, "men_apparel or womens_apprel must be carried")
+    end
+  end
 
 end
